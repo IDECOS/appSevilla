1e26ce8ef99e2ba0d9d5b36b31c7cd7f2d250f1b
jaghomix <soloideasmix@gail.co>
2021-12-13 21:56:32 -0500

    Creando modelo de datos

--- app/src/main/java/com/example/appsevilla/MainActivity.kt
+++ app/src/main/java/com/example/appsevilla/MainActivity.kt
@@ -6,17 +6,19 @@ import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.appsevilla.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     private lateinit var view: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val navController = this.findNavController(R.id.myNavHostFragment)
 
         NavigationUI.setupActionBarWithNavController(this, navController)
